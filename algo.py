179a80cf651cfbb0aa1399f4fbcbbce9782408df
adityabatheja <batheja.a@husky.neu.edu>
2017-11-26 21:06:45 -0500

    final commit

--- algo.py
+++ algo.py
@@ -1,11 +1,9 @@
 import numpy as np
 import scipy.io
-import random
 import matplotlib.pyplot as plt
-from sklearn import svm
 
 
-def pca(y):
+def pca(y, d):
     mean = np.zeros((y.shape[0], 1))
     for i in range(y.shape[1]):
         mean += y[:, i]/y.size
@@ -14,36 +12,42 @@ def pca(y):
         temp = y[:, i] - mean
         centred_y[:, i] = temp.T
     u, s, v = np.linalg.svd(centred_y)
-    print(s)
+    print(u.shape)
     plt.plot(s)
     plt.show()
-    x = np.zeros((u.shape[0], y.shape[1]))
-    # for i in range(x.shape[1]):
-    #     x[:, i] =
+    x = np.zeros((d, y.shape[1]))
+    for i in range(x.shape[1]):
+        x[:, i] = np.matmul(u[:, :d].T, (centred_y[:, i]))
     return mean, u, x
 
-def kpca(k):
-    i = np.eye(k.size)
-    print(np.ones(k.shape[1]))
-    mul = (i - (np.dot(np.ones(k.shape[1]), np.ones(k.shape[1]).T)/k.size))
-    k_n = np.dot(np.dot(mul, k), mul)
+
+def kpca(k, d):
+    i = np.eye(k.shape[1])
+    o = np.ones(k.shape[1])/k.shape[1]
+    mul = i - o
+    k_n = np.matmul(np.matmul(mul, k), mul)
+    print(k_n.shape)
     w, v = np.linalg.eig(k_n)
-    n = np.linalg.norm(w, ord=2)
-    print(n)
+    # print(v)
+    for i in range(v.shape[1]):
+        v[:, i] = v[:, i]/w[i]
+    temp = v[:, :d].T
+    x = np.matmul(temp, k)
+    return x
+
 
 def kernel(y, sig):
     k = np.zeros((y.shape[1], y.shape[1]))
     for i in range(y.shape[1]):
         for j in range(y.shape[1]):
-            temp = -np.linalg.norm(y[:, i] - y[:, j], 2)/2*pow(sig, 2)
-            k[i, j] = np.exp(temp)
+            temp = np.linalg.norm(y[:, i] - y[:, j], ord=2)**2/(2*(sig**2))
+            k[i, j] = np.exp(-temp)
     return k
-
-
 
 
 data = scipy.io.loadmat('dataset1.mat')
 Y = np.asmatrix(data['Y'])
-ker = kernel(Y, 2)
-print(ker[1, 1])
-kpca(ker)
+ker = kernel(Y, 0.6)
+x = kpca(ker, 2)
+plt.scatter(x[0, :], x[1, :], c='blue')
+plt.show()
